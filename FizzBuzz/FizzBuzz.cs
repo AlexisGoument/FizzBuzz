be37b4ce9e96361fdd30348f6ad07a748905af21
Alexis <alexis.nguyen-goument@primeum.com>
2023-10-20 11:17:08 +0200

    kata solution + tests

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,7 +4,27 @@
     {
         public string[] CountTo100()
         {
-            throw new NotImplementedException();
+            var results = new List<string>();
+
+            for (int nb = 1; nb <= 100; ++nb)
+            {
+                if (IsMultiple3(nb) && IsMultiple5(nb)) results.Add("FizzBuzz");
+                else if (IsMultiple3(nb)) results.Add("Fizz");
+                else if (IsMultiple5(nb)) results.Add("Buzz");
+                else results.Add(nb.ToString());
+            }
+
+            return results.ToArray();
+        }
+        
+        protected bool IsMultiple3(int nb)
+        {
+            return nb % 3 == 0;
+        }
+
+        protected bool IsMultiple5(int nb)
+        {
+            return nb % 5 == 0;
         }
     }
 }
--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -1,16 +1,74 @@
 namespace FizzBuzzTests
 {
+    public class DummyFizzBuzz : FizzBuzz.FizzBuzz
+    {
+        public new bool IsMultiple3(int nb)
+        {
+            return base.IsMultiple3(nb);
+        }
+
+        public new bool IsMultiple5(int nb)
+        {
+            return base.IsMultiple5(nb);
+        }
+    }
+
     public class FizzBuzzTests
     {
+        private DummyFizzBuzz _fizzBuzz;
+
         [SetUp]
         public void Setup()
         {
+            _fizzBuzz = new DummyFizzBuzz();
         }
 
         [Test]
-        public void Test1()
+        public void IsMultiple3_Success()
         {
-            Assert.Pass();
+            var actual = _fizzBuzz.IsMultiple3(3);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void IsMultiple3_False()
+        {
+            var actual = _fizzBuzz.IsMultiple3(2);
+            Assert.That(actual, Is.False);
+        }
+
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(15)]
+        [TestCase(125)]
+        public void IsMultiple5_Success(int nb)
+        {
+            var actual = _fizzBuzz.IsMultiple5(5);
+            Assert.That(actual, Is.True);
+        }
+
+        [TestCase(1)]
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(101)]
+        public void IsMultiple5_False(int nb)
+        {
+            var actual = _fizzBuzz.IsMultiple5(2);
+            Assert.That(actual, Is.False);
+        }
+
+        [Test]
+        public void FizzBuzz_CountTo100_ShouldReturn100Numbers()
+        {
+            var results = _fizzBuzz.CountTo100();
+            Assert.That(results.Length, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void FizzBuzz_CountTo100_ShouldBeginBy1()
+        {
+            var results = _fizzBuzz.CountTo100();
+            Assert.That(results[0], Is.EqualTo("1"));
         }
     }
 }
