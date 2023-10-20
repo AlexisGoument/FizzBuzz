73a2248e2cbaac0dc5f61f6d4dca8a2984f69a56
Alexis <alexis.nguyen-goument@primeum.com>
2023-10-20 10:56:32 +0200

    step 0

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string[] CountTo100()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var lines = fizzBuzz.CountTo100();
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
