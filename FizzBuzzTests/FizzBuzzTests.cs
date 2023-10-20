1dbdf2ba5b465a78d29950a05e8d56708d06a3e1
Alexis <alexis.nguyen-goument@primeum.com>
2023-10-20 11:08:54 +0200

    add test project

---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+namespace FizzBuzzTests
+{
+    public class FizzBuzzTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
